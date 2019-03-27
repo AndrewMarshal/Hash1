a4630ca2f2a3f8c13580e4f6627a7e52f1993542
AndrewMarshal <37539223+AndrewMarshal@users.noreply.github.com>
2019-03-28 01:42:00 +0500

    Hash

---
+++ HashTable.Tests1/HashTable.Tests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HashTable.Tests1
+{
+    [TestClass]
+    public class TestsHashTable
+    {
+        [TestMethod]
+        public void SearshAndPut3ElementsTest() // Добавление трёх элементов, поиск трёх элементов 
+        {
+            var pair = new HashTable(3);
+            for (int i = 0; i < 3; i++)
+            {
+                pair.PutPair(i.GetHashCode(), i);
+            }
+            for (int j = 0; j < 3; j++)
+            {
+                Assert.AreEqual(j, pair.GetValueByKey(j.GetHashCode()));
+            }
+        }
+
+        [TestMethod]
+        public void Add2KeysTest() //  Добавление одного и того же ключа дважды с разными значениями сохраняет последнее добавленное значение
+        {
+            var pair = new HashTable(2);
+            string title1 = "Crysis";
+            string title2 = "Metro";
+            pair.PutPair(0, title1);
+            pair.PutPair(0, title2);
+            Assert.AreEqual(title2, pair.GetValueByKey(0));
+        }
+
+        [TestMethod]
+        public void Add10000ElementsAnd1SearchTest() //  Добавление 10000 элементов в структуру и поиск одного из них
+        {
+            var pair = new HashTable(10000);
+            for (int i = 0; i < 10000; i++)
+            {
+                pair.PutPair(i, i * 100);
+            }
+            Assert.AreEqual(700, pair.GetValueByKey(7));
+        }
+
+        [TestMethod]
+        public void Add10000ElementsAndSearch1000NotAddTest() // Добавление 10000 элементов в структуру и поиск 1000 недобавленных ключей, поиск которых должен вернуть null
+        {
+            var pair = new HashTable(10000);
+            for (int i = 0; i < 10000; i++)
+            {
+                pair.PutPair(i, i);
+            }
+            for (int j = 10000; j < 10000 + 1000; j++)
+            {
+                Assert.AreEqual(pair.GetValueByKey(j), null);
+            }
+        }
+    }
+}
---
+++ HashTable/HashTable.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashTable
+{
+    public class Pair
+    {
+        public object Key { get; private set; }
+
+        public object Value { get; private set; }
+
+        public Pair AttachedPair;
+
+        public Pair(object key, object value)
+        {
+            Key = key;
+            Value = value;
+            if (Key is null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+            if (Value is null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+        }
+
+        public void TransformValue(object value)
+        {
+            Value = value;
+        }
+    }
+
+    public class HashTable
+    {
+        public Pair[] array;
+        readonly int size;
+        /// <summary>
+        /// Конструктор контейнера
+        /// summary>
+        /// size">Размер хэш-таблицы
+        public HashTable(int size)
+        {
+            this.size = size;
+            array = new Pair[size];
+        }
+
+        ///
+        /// Метод складывающий пару ключь-значение в таблицу
+        ///
+        /// key">
+        /// value">
+        public void PutPair(object key, object value) // Метод складывающий пару ключь-значение в таблицу
+        {
+            var hash = key.GetHashCode();
+            hash = Math.Abs(hash) % size;
+            var str = new Pair(key, value);
+            var oldStr = array[hash];
+            if (oldStr is null)
+            {
+                array[hash] = str;
+            }
+            else
+            {
+                while (true)
+                {
+                    if (oldStr.Key.Equals(key))
+                    {
+                        oldStr.TransformValue(value);
+                        break;
+                    }
+                    if (oldStr.AttachedPair is null)
+                    {
+                        oldStr.AttachedPair = str;
+                        break;
+                    }
+                    else
+                    {
+                        oldStr = oldStr.AttachedPair;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Поиск значения по ключу
+        /// summary>
+        /// key">Ключь
+        /// <returns>Значение, null если ключ отсутствует <returns>
+        public object GetValueByKey(object key) // 
+        {
+            foreach (var e in array)
+            {
+                if (e.Key.Equals(key))
+                    return e.Value;
+            }
+            return null;
+        }
+    }
+}
